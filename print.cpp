1b263a28449fdb196a4e8ddde140fed2b4907e68
valtosw <146668025+valtosw@users.noreply.github.com>
2024-07-16 20:33:36 +0300

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -2,8 +2,7 @@
 #include "print.h"
 
 void logging::printInfo() {
-    std::cout << "Bessarabov Volodymyr  K-14\n";
-    std::cout << "Variant 10: Do two quadrangles intersect?\n";
+    std::cout << "Do two quadrangles intersect?\n";
 }
 
 void logging::result(bool intersection){
